9bf37f378f5e58d2b98dc12f43cc24f709719cad
Angad Grewal <angadgrewal31@gmail.com>
2020-03-31 20:43:31 +0000

    assertEqual commit

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+const assertEqual = function(actual, expected) {
+  let passed = "✅ Function Passed: " + actual + " === " + expected;
+  let failed = "❌ Function Failed: " + actual + " !== " + expected;
+  actual === expected ? console.log(passed) : console.log(failed);
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("LHL", "LHL");
+assertEqual(1, '1');
+assertEqual(1, "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(true, 1);
+assertEqual(false, false);
