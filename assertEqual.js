29b2217dfba0d335b92ef26f09958857ba135bd8
Angad Grewal <angadgrewal31@gmail.com>
2020-03-31 20:52:24 +0000

    made code tidy with using template strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,6 +1,6 @@
 const assertEqual = function(actual, expected) {
-  let passed = "✅ Function Passed: " + actual + " === " + expected;
-  let failed = "❌ Function Failed: " + actual + " !== " + expected;
+  let passed = `✅ Function Passed: ${actual} === ${expected}`;
+  let failed = `❌ Function Failed: ${actual} !== ${expected}`;
   actual === expected ? console.log(passed) : console.log(failed);
 };
 
