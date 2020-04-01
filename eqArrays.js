4c108dbdc2f016232c60f1d2d45bdfc2e48b94d6
Angad Grewal <angadgrewal31@gmail.com>
2020-04-01 21:41:54 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  let passed = `✅ Function Passed: ${actual} === ${expected}`;
+  let failed = `❌ Function Failed: ${actual} !== ${expected}`;
+  actual === expected ? console.log(passed) : console.log(failed);
+};
+
+const eqArrays = function(inputArray, testArray) {
+  if (inputArray.length === testArray.length) {
+    for (let i = 0; i < inputArray.length; i++) {
+      if (inputArray[i] !== testArray[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
