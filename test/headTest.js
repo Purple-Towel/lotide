9f3014c0a16882ba6923686a45291c54f35f9308
Angad Grewal <angadgrewal31@gmail.com>
2020-04-06 21:36:58 +0000

    added mocha and chai, refactored headTest.js again

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,14 @@
 let head = require('../head');
-let assertEqual = require('../assertEqual')
-assertEqual(head([]), 5);
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+let assert = require('chai').assert;
+
+describe("#head", () => {
+  it('returns 1 for [1, 2, 3]', () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), 'Hello'); 
+  });
+});
+//assertEqual(head([]), 5);
+//assertEqual(head([5,6,7]), 5);
+//assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
