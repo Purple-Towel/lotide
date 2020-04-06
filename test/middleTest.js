cb655d11c3b80e77390aca858c423434d6d21802
Angad Grewal <angadgrewal31@gmail.com>
2020-04-06 22:13:07 +0000

    refactored tail and middle tests to use chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,22 @@
 let middle = require('../middle');
-let assertArraysEqual = require('../assertArraysEqual');
+let assert = require('chai').assert;
 
+describe("#middle", () => {
+  it('returns 2 for [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs"]), ['Lighthouse']); 
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs', 'Students']", () => {
+    assert.deepEqual(middle(["Hello", "Lighthouse", "Labs", "Students"]), ['Lighthouse', 'Labs']); 
+  });
+});
+
+/*
 assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
 assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
 assertArraysEqual(middle([1, 2, 3, 4,]), [2, 3]);
@@ -9,3 +25,4 @@ assertArraysEqual(middle([1, 2]), []);
 assertArraysEqual(middle([1, 2]), [1]);
 assertArraysEqual(middle([1]), []);
 assertArraysEqual(middle([1]), [1]);
+*/
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,22 @@
 let tail = require('../tail');
-let assertEqual = require('../assertEqual');
+let assert = require('chai').assert;
 
+describe("#tail", () => {
+  it('returns 2 for [1, 2, 3]', () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ['Lighthouse', "Labs"]); 
+  });
+  it('returns [2, 3] for [1, 2, 3, 4]', () => {
+    assert.deepEqual(tail([1, 2, 3, 4]), [2, 3, 4]);
+  });
+  it("returns 'Lighthouse' for ['Hello', 'Lighthouse', 'Labs', 'Students']", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs", "Students"]), ['Lighthouse', 'Labs', 'Students']); 
+  });
+});
+
+/*
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(tail(words).length, 2); // original array should still have 3 elements!
@@ -14,3 +30,4 @@ assertEqual(result[1], "Labs"); // ensure second element is "Labs"
 // Test Case 2: ...
 assertEqual(tail(['one']).length, [].length); // if 1 item in array, output empty array
 assertEqual(tail([]).length, [].length); // if empty array, return empty empty array
+*/
